e166477284b1f429661f7aac848a43ec5fff0c7b
sandesh-dalvi <dalvi12sandesh08@gmail.com>
2026-03-31 15:48:54 +0530

    feat: initial commit

---
+++ components/navbar.tsx
@@ -0,0 +1,19 @@
+import { Briefcase } from "lucide-react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className=" border-b border-gray-200 bg-white">
+      <div className=" container mx-auto flex h-16 items-center px-4 ">
+        <Link
+          href="/"
+          className=" flex items-center gap-2 text-xl font-semibold text-primary"
+        >
+          <Briefcase /> Job Tracker
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
